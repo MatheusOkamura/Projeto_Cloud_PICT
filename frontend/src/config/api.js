5ff83f44a5f2c8a1bfd65fc8be6364b50181e940
lufe70 <lufebarreto@gmail.com>
2025-11-23 14:10:37 -0300

    Atualiza URLs de API e configurações de CORS; adiciona permissões de execução

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
 // Configuração centralizada da API
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://pictback-bzakbsfgc6bgcjcc.brazilsouth-01.azurewebsites.net/api';
-
+//const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://pictback-bzakbsfgc6bgcjcc.brazilsouth-01.azurewebsites.net/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://pictibmec-dmh5czbrd0bkcze5.brazilsouth-01.azurewebsites.net/api';
 export default API_BASE_URL;
